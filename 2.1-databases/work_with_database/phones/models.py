1d8eacc8e892280a4c0641e4ba266ee07ef16ef0
AndreyPotapovAndrey <125595526+AndreyPotapovAndrey@users.noreply.github.com>
2023-08-10 17:05:39 +0300

    Update models.py

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -9,18 +9,3 @@ class Phone(models.Model):
     release_date = models.DateField()
     lte_exist = models.BooleanField()
     slug = models.SlugField(max_length=50, unique=True)
-
-
-# from django.db import models
-#
-#
-# class Phone(models.Model):
-#     name = models.CharField(max_length=50)
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     image = models.ImageField()
-#     release_date = models.DateField()
-#     lte_exist = models.BooleanField()
-#     slug = models.SlugField(max_length=225, unique=True, db_index=True)
-#
-#     def __str__(self):
-#         return self.name
