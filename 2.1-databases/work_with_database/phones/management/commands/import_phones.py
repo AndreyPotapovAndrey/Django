3676d9582d309194c7f6a909e2454d37b78da365
AndreyPotapovAndrey <125595526+AndreyPotapovAndrey@users.noreply.github.com>
2023-08-10 17:06:46 +0300

    Update import_phones.py

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -25,29 +25,3 @@ class Command(BaseCommand):
                 lte_exist=phone.get('lte_exists'),
                 slug=slugify(phone.get('name'))
             )
-
-
-# import csv
-#
-# from django.core.management.base import BaseCommand
-# from phones.models import Phone
-#
-#
-# class Command(BaseCommand):
-#     def add_arguments(self, parser):
-#         pass
-#
-#     def handle(self, *args, **options):
-#         with open('phones.csv', 'r') as file:
-#             phones = list(csv.DictReader(file, delimiter=';'))
-#
-#         for phone in phones:
-#             Phone.objects.create(id=phone.get('id'), name=phone.get('name'), price=phone.get('price'),
-#                                  image=phone.get('image'), release_date=phone.get('release_date'),
-#                                  lte_exists=phone.get('lte_exists'), slug=phone.get('name').lower().replace(' ', '-'))
-
-
-
-
-
-
